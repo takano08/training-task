6c45402c1eb4ecd0ca3dda45f006e2bc32b231f5
takano <ri1saxxminny83xx@gmail.com>
2020-08-11 15:32:17 +0900

    header-footer-vue

---
+++ components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footerGroup">
+    <el-button href="news">お知らせ</el-button>
+    <el-button href="about">企業情報</el-button>
+    <el-button href="blog">ブログ</el-button>
+    <el-button href="recruit">採用情報</el-button>
+    <el-button href="admin">管理者メニュー</el-button>
+  </div>
+</template>
+
+<style scoped>
+  .footerGroup {
+    text-align: right;
+
+    font-weight: lighter ;
+  }
+  .footerGroup a{
+    color: #5B5B5B;
+  }
+</style>
---
+++ components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="headerGroup">
+    <el-button href="news">お知らせ</el-button>
+    <el-button href="about">企業情報</el-button>
+    <el-button href="blog">ブログ</el-button>
+    <el-button href="recruit">採用情報</el-button>
+    <el-button href="admin">管理者メニュー</el-button>
+  </div>
+</template>
+
+<style scoped>
+  .headerGroup {
+    text-align: right;
+    //font-family: 'Lato' !important;
+    font-weight: lighter ;
+  }
+  .headerGroup a{
+    color: #5B5B5B;
+  }
+</style>
